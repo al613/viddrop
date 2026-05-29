0592da94c38398417f376c1c5ec1595c721fcc39
al613 <aysar1813@gmail.com>
2026-05-29 11:27:06 +0300

    Fix build script for yt-dlp

--- build.sh
+++ build.sh
@@ -3,4 +3,10 @@ set -o errexit
 
 npm install
 
-python3 -m pip install --user yt-dlp
+python3 -m pip install --user -U yt-dlp
+
+mkdir -p bin
+cp "$(python3 -m site --user-base)/bin/yt-dlp" ./bin/yt-dlp
+chmod +x ./bin/yt-dlp
+
+./bin/yt-dlp --version
